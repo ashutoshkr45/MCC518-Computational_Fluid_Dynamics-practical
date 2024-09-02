74ccfa2165a199d41103db1a6ffefae3ba52f15d
Ashutosh Kumar <154620922+ashutoshkr45@users.noreply.github.com>
2024-09-02 11:32:31 +0530

    Add files via upload

---
+++ 06_semi-implicit_heat_ii.cpp
@@ -0,0 +1,69 @@
+#include<iostream>
+#include<iomanip>
+using namespace std;
+float x[10];
+
+float *tridiagonal(float r, float d[],int n){
+    int i;
+    float a=-r/2,b=(1+r),c=-r/2;
+    float alpha[10],beta[10];
+
+    alpha[1]=b;
+    beta[1]=d[1]/b;
+
+    for(i=2;i<n;i++){
+        alpha[i]=b-(a*c/alpha[i-1]);
+        beta[i]=(d[i]-a*beta[i-1])/alpha[i];
+    }
+    x[n]=beta[n];
+    for(i=n-1;i>=1;i--)
+        x[i]=beta[i]-(c*x[i+1])/alpha[i];
+    return x;
+}
+
+float f1(float x){
+	return 2 * x;
+}
+float f2(float x){
+	return 2 * (1 - x);
+}
+
+int main(){
+    int i,j,n,m;
+    float u[100][100],h=0.1,k=0.01,c=1,r;
+    r = (k * c * c) / (h * h);
+    m = 1;
+    n = 10;
+
+    cout<<"The value of Diffusion coefficient(r) is "<< r <<endl;
+
+    for(j=0;j<=m;j++){
+        u[0][j]=0;
+        u[n][j]=0;
+    }
+    
+    for(i=1;i<n;i++){
+    	if(i <= 5)
+    		u[i][0]=f1(i*h);
+    	else
+    		u[i][0]=f2(i*h);
+	}
+
+    float d[n]={0};
+    
+    for(j=0;j<m;j++){
+        for(i=1;i<n;i++)
+            d[i]=(r/2)*u[i+1][j]+(1-r)*u[i][j]+(r/2)*u[i-1][j];
+        float *y=tridiagonal(r,d,n);
+        for(i=1;i<n;i++)
+            u[i][j+1]=y[i];
+    }
+    
+    cout<<"The value of all u(i,j): \n";
+    cout<<fixed<<setprecision(2);
+    for(j=m;j>=0;j--){
+        for(i=0;i<=n;i++)
+           cout<<"\t"<<u[i][j];
+       cout<<endl;
+    }
+}
