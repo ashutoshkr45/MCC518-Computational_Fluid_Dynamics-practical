5e6d1f7658243d64b27c3ae0cbde5bef1cb4ae67
Ashutosh Kumar <154620922+ashutoshkr45@users.noreply.github.com>
2024-09-28 23:26:35 +0530

    Update 07_laplace_eqn.cpp

--- 07_laplace_eqn.cpp
+++ 07_laplace_eqn.cpp
@@ -3,53 +3,60 @@ using namespace std;
 
 int main(){
     int i,j,n,iter,k;
-    float u[10][10];
+    float u[10][10], u_prev[10][10];
     cout<<"Enter no. of mesh points, no. of iterations : ";
     cin>>n>>iter;
     
-    cout<<"Enter the boundary values u(i,0):-\n";
-    for(i=0; i<=n; i++)
-    {
+    for(i=0; i<=n; i++){
+        cout<<"Enter u("<<i<<",0): ";
         cin>>u[i][0];
     }
-    cout<<"Enter the boundary values u(i,"<<n<<"):-\n";
-    for(i=0;i<n+1;i++)
-    {
+    for(i=0; i<=n; i++){
+        cout<<"Enter u("<<i<<","<<n<<"): ";
         cin>>u[i][n];
     }
-    cout<<"Enter the boundary values u(0,j):-\n";
-    for(j=0;j<n+1;j++)
-    {
+    for(j=1; j<n; j++){
+        cout<<"Enter u(0,"<<j<<"): ";
         cin>>u[0][j];
     }
-    cout<<"Enter the boundary values u("<<n<<",j) except corners:-\n";
-    for(j=0;j<n+1;j++)
-    {
+    for(j=1; j<n; j++){
+        cout<<"Enter u("<<n<<","<<j<<"): ";
         cin>>u[n][j];
     }
-    for(i=1; i<n; i++)
-    {
-        for(j=1; j<n; j++)
-        {
+
+    for(i=1; i<n; i++){
+        for(j=1; j<n; j++){
             u[i][j] = 0;
         }
     }
-    for(k=1;k<=iter;k++)
-    {
-    	if(k==1)
+
+    for(i = 0; i <= n; i++)
+        for(j = 0; j <= n; j++)
+            u_prev[i][j] = u[i][j];
+
+    for(k=1;k<=iter;k++){
+    	if(k==1){
     		u[1][2] = (u[0][3] + u[2][3] + u[0][1] + u[2][1]) / 4;
     		
-        for(i=1;i<n;i++)
-        {
-            for(j=n-1;j>0;j--)
-            {
-            	if(i==1 && j==2)
-            		continue;
-            	else
-                	u[i][j] = (u[i-1][j] + u[i+1][j] + u[i][j-1] + u[i][j+1])/4;
+            for(i=1;i<n;i++){
+                for(j=n-1;j>0;j--){
+            	    if(k==1 && i==1 && j==2)
+            		    continue;
+            	    else
+                	    u[i][j] = (u[i-1][j] + u[i+1][j] + u[i][j-1] + u[i][j+1])/4;
+                }
             }
         }
-        printf("\n\nThe Solution of Laplace Equation is-\n");
+        else{
+            for(i=1;i<n;i++)
+                for(j=n-1;j>0;j--)
+                	u[i][j] = (u_prev[i-1][j] + u_prev[i+1][j] + u_prev[i][j-1] + u_prev[i][j+1])/4;
+        }
+        for (i = 1; i < n; i++) 
+            for (j = 1; j < n; j++) 
+                u_prev[i][j] = u[i][j];
+
+        printf("\n\nIteration - %d: \nThe Solution of Laplace Equation is-\n",k);
         for(j=n;j>=0;j--)
         {
             for(i=0;i<=n;i++)
